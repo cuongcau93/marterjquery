ecb7e43c3641c41c63b105d565d0c0b3e00acffc
cuongcau93 <cuongnm22@wru.vn>
2017-02-17 17:01:16 +0700

    ver2
    ver2

--- js_patterns.html/chapter1.js
+++ js_patterns.html/chapter1.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 //    setInterval(updateClock,1000)
 //    updateClock();
-    var clock = new Clock('#clock',-0, 'UTC');
+    var clock = new com.o2GEEK.Clock('#clock',-0, 'UTC');
+    var clock2 = new com.o2GEEK.TextClock('#clock2',-0, 'UTC');
+
     //var clock2 = new creatClock('#clock2',+300,' UTCC');
     
 //    var d = new Date();
@@ -86,8 +88,10 @@ $(document).ready(function(){
 //}
 
 //5 CREAT Method with the prototype
-
-function Clock(id, offset, label){
+//Namespaces
+var com = com || {};
+    com.o2GEEK = com.o2GEEK || {};
+com.o2GEEK.Clock = function(id, offset, label){
     offset = offset || 0;
     label = label || '';
     var a = 10;
@@ -95,44 +99,78 @@ function Clock(id, offset, label){
     var d = new Date();
     var offset = (offset + d.getTimezoneOffset() + 420)*60*1000;
     this.d = new Date(offset + d.getTime());
-    this.d.autoClock();
     this.id = id;
     this.label = label;
-    var $that = this
+    var $that = this;
+    this.d.autoClock(true);
+    //console.log(Date);
     setInterval(function(){
         $that.updateClock();
     },1000);    
-//    setInterval(this.updateClock(),1000)
     this.updateClock();
+    LiveDate.call(clock,1,2,3);
+    //LiveDate.apply(clock,clock, [1,2,3]);
 }
 
+function LiveDate(a,b,c){
+    //console.log(this,this,a,b,c);
+}
+
+Date.__interval = 0;
+Date.__aDates = [];
+
+Date.addToInterval = function(date){
+    this.__aDates.push(date);
+    if(!Date.__interval){
+        Date.__interval = setInterval(function(){Date.updateDates()}, 1000)
+    }
+}
+Date.updateDates = function(){
+    for(var i=0; i < this.__aDates.length; i++){
+        this.__aDates[i].updateSeconds();
+    }
+}
 Date.prototype.updateSeconds = function(){
     this.setSeconds(this.getSeconds() + 1);
 }
-
-Date.prototype.autoClock(isAuto){
-    clearInterval();
+Date.prototype.autoClock = function(isAuto){
     if(isAuto){
-        var that = this;
-        this.clockInterval = setInterval(function(){that.updateSeconds()},1000)
+        Date.addToInterval(this)
     }
 }
 
-Clock.prototype.updateClock = function(){
+com.o2GEEK.Clock.prototype.Version = '1';
+
+com.o2GEEK.Clock.prototype.updateClock = function(){
     var date = this.d;
-    date.updateSeconds();
-    //date = new Date(this.offset + date.getTime());
-    
     var clock = $(this.id);
     clock.text(this.formatDigits(date.getHours()) +":"+ this.formatDigits(date.getMinutes()) +":"+ this.formatDigits(date.getSeconds()) + " " + this.label +" "+ this.a);
 }
-Clock.prototype.formatDigits = function(val){
+
+com.o2GEEK.Clock.prototype.formatDigits = function(val){
       if(val < 10){
            val = "0" + val;
        }
        return val;  
 }
 
+com.o2GEEK.TextClock = function(id, offset, label){
+    com.o2GEEK.Clock.apply(this, arguments);
+}
+
+com.o2GEEK.TextClock.prototype = createObject(com.o2GEEK.Clock.prototype, com.o2GEEK.TextClock);
+//com.o2GEEK.TextClock.prototype.constructor = com.o2GEEK.TextClock;
+
+//create function by polyfill
+function createObject(proto, cons){
+    function c(){};
+    console.log(cons);
+    console.log(cons);
+    c.prototype = proto;
+    c.prototype.constructor = cons;
+    console.log(new c());
+    return new c();
+}
 
 
 
