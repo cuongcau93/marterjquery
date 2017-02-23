fbcc4ab7c70de954764d59b0f47f7a161630f42e
cuongcau93 <cuongnm22@wru.vn>
2017-02-24 00:04:30 +0700

    call vs apply
    call vs apply

--- js_patterns.html/book_patterns/OOP.js
+++ js_patterns.html/book_patterns/OOP.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
     //PRIVATE variable
-    var a = new Restaurant();
-    a.use_restroom();  
-    a.name;
-    a.age();
-    var b = new myMain();
-    a.age = function(){
-        this.bb = 'cuong';
-        console.log(this.bb);
-    }
-    a.age();
+    // var a = new Restaurant();
+    // a.use_restroom();  
+    // a.name;
+    // a.age();
+    // var b = new myMain();
+    // a.age = function(){
+    //     this.bb = 'cuong';
+    //     console.log(this.bb);
+    // }
+    // a.age();
     
-    var emp = [], i;
-    for (i = 0; i < 3; i++) {
-        emp[i] = new Employee();
-        console.log("employee count is "+ emp[i].getCount());
-    }
+    var call = new Food('Na', -2);
+    // var emp = [], i;
+    // for (i = 0; i < 3; i++) {
+    //     emp[i] = new Employee();
+    //     console.log("employee count is "+ emp[i].getCount());
+    // }
 })
 function Employee() {
     this.getCount = function(){
@@ -72,3 +73,27 @@ function myMain(){
     }
 }
 
+//Dung ham call để gán giá trị cho hàm khởi tạo
+
+function Product(name, price) {
+  this.name = name;
+  this.price = price;
+  console.log(name + ' ' + price)
+}
+function Food(name, price) {
+  Product.apply(this,[name, price]);
+  this.category = 'food';
+}
+function Toy(name, price) {
+  Product.call(this, name, price);
+  this.category = 'toy';
+}
+
+//dùng hàm call để gọi hàm vô danh
+(function(name){
+    console.log(name)
+}).apply(this, ['Nguyen Manh Cường']);
+//Call là hàm đơn giản, dễ hiểu nhất trong bộ 3 khó nhằn này. Mục tiêu của nó chỉ là mình thực hiện lời gọi hàm như thông thường, nhưng 
+//điều khác biệt ở đây so với những lời gọi hàm thông thường là 
+//mình có thể xác định ngữ cảnh bên trong hàm được gọi (thay vì mặc định là caller hiện hành).
+//Dùng để thay đổi biến this
